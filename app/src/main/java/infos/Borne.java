ca99966a0caf49a217300a01fd66b361523c25e8
Eddi3As <eddi3.as@gmail.com>
2018-10-03 00:56:22 -0400

    Ajout de classe client et borne

---
+++ app/src/main/java/infos/Borne.java
@@ -0,0 +1,14 @@
+package infos;
+
+public class Borne {
+
+    private String nom;
+    private String address;
+    private float longitude;
+    private float latitude;
+    private String vitesse;
+
+    public Borne(){
+
+    }
+}
---
+++ app/src/main/java/infos/Client.java
@@ -0,0 +1,15 @@
+package infos;
+
+public class Client {
+
+    private String username;
+    private String password;
+    private String prenom;
+    private String nom;
+    private String ville;
+    private String email;
+
+    public Client(){
+
+    }
+}
